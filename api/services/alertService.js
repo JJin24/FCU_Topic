ac4a72ea9acc309e3490b80d7e84b45c6b29885f
JinTing <lijinting32@gmail.com>
2025-09-04 22:54:31 +0800

    Add hostname in alert
    在 alert "get" 相關的程式中添加了 hostname 的欄位。這一個 hostname
    取得的方式是先將 flow.src_ip 及 flow.dst_ip 先做 UNION 後與 host.ip
    做運算取得 hostname 後再去與 flow 或是 alert_history
    做結合，以取得相關的的結果。
    
    **同一個 flow 如果 src_ip 與 dst_ip 都有被記錄在 host
    中的話，其會顯示兩筆的資訊在此**

--- api/services/alertService.js
+++ api/services/alertService.js
@@ -6,12 +6,26 @@ async function getAllAlert() {
     conn = await pool.getConnection();
 
     const alert_history = await conn.query(
-      "SELECT uuid, timestamp, src_ip, src_port, dst_ip, dst_port, protocol_list.name AS protocol, label_list.name AS label, score, status \
-      FROM alert_history \
-      LEFT JOIN label_list ON alert_history.label = label_list.label_id  \
-      LEFT JOIN flow ON alert_history.id = flow.id \
+      "WITH RelevantHostIPs AS ( \
+          SELECT src_ip AS ip FROM flow \
+          UNION \
+          SELECT dst_ip AS ip FROM flow \
+      ), \
+      HostIPToNameMap AS ( \
+          SELECT rh.ip, host.name AS hostname \
+          FROM RelevantHostIPs AS rh \
+          INNER JOIN host ON rh.ip = host.ip \
+      ) \
+      SELECT flow.uuid, COALESCE(h_src.hostname, h_dst.hostname) AS hostname, \
+      flow.timestamp, flow.src_ip, flow.src_port, flow.dst_ip, flow.dst_port, \
+      protocol_list.name AS protocol, label_list.name AS label, ah.score, ah.status \
+      FROM alert_history AS ah \
+      LEFT JOIN flow ON ah.id = flow.id \
+      LEFT JOIN label_list ON ah.label = label_list.label_id \
       LEFT JOIN protocol_list ON flow.protocol = protocol_list.protocol \
-      WHERE alert_history.id = flow.id;"
+      LEFT JOIN HostIPToNameMap AS h_src ON flow.src_ip = h_src.ip \
+      LEFT JOIN HostIPToNameMap AS h_dst ON flow.dst_ip = h_dst.ip \
+      WHERE ah.status = 0;"
     )
     console.log(alert_history);
     return alert_history;;
@@ -30,12 +44,26 @@ async function getNotDealAlert(){
     conn = await pool.getConnection();
 
     const topXDayFlow = await conn.query(
-      "SELECT uuid, timestamp, src_ip, src_port, dst_ip, dst_port, protocol_list.name AS protocol, label_list.name AS label, score, status \
+      "WITH RelevantHostIPs AS ( \
+      SELECT src_ip AS ip FROM flow \
+      UNION \
+      SELECT dst_ip AS ip FROM flow \
+      ), \
+      HostIPToNameMap AS ( \
+          SELECT rh.ip, host.name AS hostname \
+          FROM RelevantHostIPs AS rh \
+          INNER JOIN host ON rh.ip = host.ip \
+      ) \
+      SELECT flow.uuid, COALESCE(h_src.hostname, h_dst.hostname) AS hostname, \
+      flow.timestamp, flow.src_ip, flow.src_port, flow.dst_ip, flow.dst_port, \
+      protocol_list.name AS protocol, label_list.name AS label, ah.score, ah.status \
       FROM flow \
-      LEFT JOIN alert_history ON flow.id = alert_history.id \
-      LEFT JOIN label_list ON alert_history.label = label_list.label_id  \
+      LEFT JOIN alert_history AS ah ON flow.id = ah.id \
+      LEFT JOIN label_list ON ah.label = label_list.label_id \
       LEFT JOIN protocol_list ON flow.protocol = protocol_list.protocol \
-      WHERE status = 0;"
+      LEFT JOIN HostIPToNameMap AS h_src ON flow.src_ip = h_src.ip \
+      LEFT JOIN HostIPToNameMap AS h_dst ON flow.dst_ip = h_dst.ip \
+      WHERE ah.status = 0;"
     )
     console.log(topXDayFlow);
     return topXDayFlow;
